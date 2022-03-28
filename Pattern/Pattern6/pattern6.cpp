ac5559d5e7d12626a3d9964e3dc260a96fc79584
Sourav Pandey <60563930+souravpandey33@users.noreply.github.com>
2022-03-28 21:23:38 +0530

    Add files via upload

---
+++ Pattern/Pattern6/pattern6.cpp
@@ -0,0 +1,39 @@
+
+// 12345
+// 1234
+// 123
+// 12
+// 1
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+void pattern(int n)
+{
+    int i,j;
+    for(i=-n;i<=n;i++)
+    {
+        for(j=1;j<=abs(i);j++)
+        {
+            cout<<j;
+        }
+        if(i==0)
+            continue;
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    int x;
+    cout<<"Enter Size : "<<endl;
+    cin>>x;
+    pattern(x);
+    return 0;
+}
