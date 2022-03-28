7e8bd45e2654c89e0743acd6c2ad26cba4557020
Sourav Pandey <60563930+souravpandey33@users.noreply.github.com>
2022-03-28 21:16:04 +0530

    Add files via upload

---
+++ Pattern/Pattern3/pattern3.cpp
@@ -0,0 +1,32 @@
+
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+void pattern(int n)
+{
+    int i,j;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    int x;
+    cout<<"Enter Size : "<<endl;
+    cin>>x;
+    pattern(x);
+    return 0;
+}
